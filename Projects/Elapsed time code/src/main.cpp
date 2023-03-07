aed98efadff2cb64cbccb8bf147b0430d0776696
rud-lucien <lucienrudkarly@gmail.com>
2023-03-07 14:38:29 -0500

    update elapsed time and serial relay code to deal with serial flush and add color to display

--- Projects/Elapsed time code/src/main.cpp
+++ Projects/Elapsed time code/src/main.cpp
@@ -7,6 +7,60 @@ byte minutes;
 byte hours;
 byte days;
 
+void redText()
+{
+  M5.Lcd.setTextColor(RED, BLACK);
+}
+
+void yellowText()
+{
+  M5.Lcd.setTextColor(YELLOW, BLACK);
+}
+
+void greenText()
+{
+  M5.Lcd.setTextColor(GREEN, BLACK);
+}
+
+void orangeText()
+{
+  M5.Lcd.setTextColor(ORANGE, BLACK);
+}
+
+void blueText()
+{
+  M5.Lcd.setTextColor(BLUE, BLACK);
+}
+
+void purpleText()
+{
+  M5.Lcd.setTextColor(PURPLE, BLACK);
+}
+
+void TextColortoDefault()
+{
+  M5.Lcd.setTextColor(WHITE, BLACK);
+}
+
+void textColor()
+{
+  if (seconds > 15 && seconds <= 30)
+  {
+    yellowText();
+  }
+  else if (seconds > 30 && seconds <= 45)
+  {
+    orangeText();
+  }
+  else if (seconds > 45 && seconds <= 60)
+  {
+    redText();
+  }
+  else
+  {
+    TextColortoDefault();
+  }
+}
 
 void setup()
 {
@@ -15,6 +69,7 @@ void setup()
   M5.Lcd.setCursor(10, 40);
   M5.Lcd.println("M5 has been running for:");
 }
+
 void loop()
 {
   if (millis() >= (previousTime))
@@ -36,19 +91,21 @@ void loop()
       hours = 0;
       days = days + 1;
     }
-    
-    M5.Lcd.setCursor(10, 50);
+
+    textColor();
+    M5.Lcd.setCursor(10, 30);
     M5.Lcd.print(days, DEC);
     M5.Lcd.print(" Days|");
     M5.Lcd.print(" ");
     M5.Lcd.print(hours, DEC);
-    M5.Lcd.print(" Hours|");
+    M5.Lcd.printf(" Hours|");
     M5.Lcd.print(" ");
     M5.Lcd.print(minutes, DEC);
     M5.Lcd.print(" Minutes|");
     M5.Lcd.print(" ");
     M5.Lcd.print(seconds, DEC);
-    M5.Lcd.print(" Seconds");
+    M5.Lcd.print(" Seconds ");
+
     // Serial.print(days, DEC);
     // Serial.print(":");
     // Serial.print(hours, DEC);
@@ -56,7 +113,6 @@ void loop()
     // Serial.print(minutes, DEC);
     // Serial.print(":");
     // Serial.println(seconds, DEC);
-    
+
   } // end 1 second
 } // end loop
-
--- Projects/Serial_relay_code_v3/src/main.cpp
+++ Projects/Serial_relay_code_v3/src/main.cpp
@@ -9,17 +9,73 @@
 //	<10> : relay 2 on
 //	<11> : both relays on
 //  <f>  : flush Serial1 write
-unsigned long previousTime = 0;
-byte seconds;
-byte minutes;
-byte hours;
-byte days;
+
 const int RELAY_1 = 32; // the board pin, which connects to the IN pin of relay 1
 const int RELAY_2 = 33; // the board pin, which connects to the IN pin of relay 2
 const unsigned long timeout_ms = 20;
 const byte numChars = 32;
 char receivedChars[numChars];
 boolean newData = false;
+
+// cursor positions for different text on screen
+const int cusor_1x = 0;
+const int cusor_1y = 20;
+
+const int cusor_2x = 0;
+const int cusor_2y = 50;
+
+const int cusor_3x = 0;
+const int cusor_3y = 65;
+
+const int cusor_4x = 0;
+const int cusor_4y = 100;
+
+const int cusor_5x = 0;
+const int cusor_5y = 110;
+
+const int cusor_6x = 30;
+const int cusor_6y = 130;
+
+unsigned long previousTime = 0;
+byte seconds;
+byte minutes;
+byte hours;
+byte days;
+
+void redText()
+{
+  M5.Lcd.setTextColor(RED, BLACK);
+}
+
+void yellowText()
+{
+  M5.Lcd.setTextColor(YELLOW, BLACK);
+}
+
+void greenText()
+{
+  M5.Lcd.setTextColor(GREEN, BLACK);
+}
+
+void orangeText()
+{
+  M5.Lcd.setTextColor(ORANGE, BLACK);
+}
+
+void blueText()
+{
+  M5.Lcd.setTextColor(BLUE, BLACK);
+}
+
+void purpleText()
+{
+  M5.Lcd.setTextColor(PURPLE, BLACK);
+}
+
+void textColortoDefault()
+{
+  M5.Lcd.setTextColor(WHITE, BLACK);
+}
 
 void setup()
 {
@@ -32,18 +88,47 @@ void setup()
   digitalWrite(RELAY_2, LOW);                // Set relay 2 to off at startup
   M5.Lcd.print("Relay Control with Serial RS-232: v4");
   M5.Lcd.drawLine(0, 10, 320, 10, BLUE);
-  M5.Lcd.setCursor(10, 20);
+  M5.Lcd.setCursor(cusor_2x, cusor_2y);
   M5.Lcd.println("M5 has been running for:");
-  M5.Lcd.setCursor(10, 70);
-  M5.Lcd.print("RELAY_1 STATE IS NOW: OFF");
-  M5.Lcd.setCursor(10, 80);
-  M5.Lcd.print("RELAY_2 STATE IS NOW: OFF");
-  M5.Lcd.setCursor(10, 50);
+  M5.Lcd.drawLine(0, 60, 140, 60, YELLOW);
+  M5.Lcd.setCursor(cusor_4x, cusor_4y);
+  M5.Lcd.print("RELAY_1 STATE IS NOW:");
+  redText();
+  M5.Lcd.print(" OFF");
+  M5.Lcd.setCursor(cusor_5x, cusor_5y);
+  textColortoDefault();
+  M5.Lcd.print("RELAY_2 STATE IS NOW:");
+  redText();
+  M5.Lcd.print(" OFF");
+  M5.Lcd.setCursor(cusor_1x, cusor_1y);
+  textColortoDefault();
+  greenText();
   M5.Lcd.print("<M5 is ready for relay commands>");
+  textColortoDefault();
   Serial1.write("<M5 is ready for relay commands>");
   Serial1.flush();
-  Serial.print("<M5 is ready for relay commands>");
-  Serial.println();
+  // Serial.print("<M5 is ready for relay commands>");
+  // Serial.println();
+}
+
+void textColor()
+{
+  if (days > 12 && days <= 24)
+  {
+    yellowText();
+  }
+  else if (days > 24 && days <= 36)
+  {
+    orangeText();
+  }
+  else if (days > 36 && days <= 49)
+  {
+    redText();
+  }
+  else
+  {
+    textColortoDefault();
+  }
 }
 
 void elapsedTime()
@@ -68,18 +153,19 @@ void elapsedTime()
       days = days + 1;
     }
 
-    M5.Lcd.setCursor(10, 30);
+    textColor();
+    M5.Lcd.setCursor(cusor_3x, cusor_3y);
     M5.Lcd.print(days, DEC);
     M5.Lcd.print(" Days|");
     M5.Lcd.print(" ");
     M5.Lcd.print(hours, DEC);
-    M5.Lcd.print(" Hours|");
+    M5.Lcd.printf(" Hours|");
     M5.Lcd.print(" ");
     M5.Lcd.print(minutes, DEC);
     M5.Lcd.print(" Minutes|");
     M5.Lcd.print(" ");
     M5.Lcd.print(seconds, DEC);
-    M5.Lcd.print(" Seconds");
+    M5.Lcd.print(" Seconds ");
 
   } // end 1 second
 }
@@ -100,12 +186,11 @@ int recvWithStartEndMarkers() // function to read from serial Receive with start
     // check to see if we timed out return 0 if all is ok return 1 if timeout occured
     if (millis() > (t_start + timeout_ms))
     {
-      M5.Lcd.setCursor(10, 90);
-      M5.Lcd.setTextColor(RED);
+      M5.Lcd.setCursor(cusor_6x, cusor_6y);
+      redText();
       M5.Lcd.print("<M5 timedout and needs a reboot>");
-      Serial.print("<M5 timedout and needs a reboot>");
+      textColortoDefault();
       Serial1.write("<M5 timedout and needs a reboot>");
-      // Serial1.flush();
       return 1;
     }
 
@@ -148,58 +233,75 @@ void showNewData()
 
     if (!strcmp(receivedChars, "11"))
     { // if <11> is sent: both relays on
-      M5.Lcd.setCursor(10, 70);
-      M5.Lcd.print("RELAY_1 STATE IS NOW:  ON");
-      M5.Lcd.setCursor(10, 80);
-      M5.Lcd.print("RELAY_2 STATE IS NOW:  ON");
+      M5.Lcd.setCursor(cusor_4x, cusor_4y);
+      M5.Lcd.print("RELAY_1 STATE IS NOW:");
+      greenText();
+      M5.Lcd.print("  ON");
+      textColortoDefault();
+      M5.Lcd.setCursor(cusor_5x, cusor_5y);
+      M5.Lcd.print("RELAY_2 STATE IS NOW:");
+      greenText();
+      M5.Lcd.print("  ON");
+      textColortoDefault();
       digitalWrite(RELAY_1, HIGH);
       digitalWrite(RELAY_2, HIGH);
-      Serial1.write("<11>"); // send a message back that shows the current state of the relay
-                             // Serial1.flush();
-      Serial.println("RELAY_1 AND RELAY_2 STATE IS NOW: ON");
+      Serial1.write("<11>");
     }
 
     if (!strcmp(receivedChars, "00"))
     { // if <00> is sent: both relays off
-      M5.Lcd.setCursor(10, 70);
-      M5.Lcd.print("RELAY_1 STATE IS NOW: OFF");
-      M5.Lcd.setCursor(10, 80);
-      M5.Lcd.print("RELAY_2 STATE IS NOW: OFF");
+      M5.Lcd.setCursor(cusor_4x, cusor_4y);
+      M5.Lcd.print("RELAY_1 STATE IS NOW:");
+      redText();
+      M5.Lcd.print(" OFF");
+      textColortoDefault();
+      M5.Lcd.setCursor(cusor_5x, cusor_5y);
+      M5.Lcd.print("RELAY_2 STATE IS NOW:");
+      redText();
+      M5.Lcd.print(" OFF");
+      textColortoDefault();
       digitalWrite(RELAY_1, LOW);
       digitalWrite(RELAY_2, LOW);
-      Serial1.write("<00>"); // send a message back that shows the current state of the relay
-                             // Serial1.flush();
-      Serial.println("RELAY_1 AND RELAY_2 STATE IS NOW: OFF");
+      Serial1.write("<00>");
     }
 
     if (!strcmp(receivedChars, "01"))
     { // if <01> is sent: relay 2 is off and relay 1 is on
-      M5.Lcd.setCursor(10, 70);
-      M5.Lcd.print("RELAY_1 STATE IS NOW:  ON");
-      M5.Lcd.setCursor(10, 80);
-      M5.Lcd.print("RELAY_2 STATE IS NOW: OFF");
+      M5.Lcd.setCursor(cusor_4x, cusor_4y);
+      M5.Lcd.print("RELAY_1 STATE IS NOW:");
+      greenText();
+      M5.Lcd.print("  ON");
+      textColortoDefault();
+      M5.Lcd.setCursor(cusor_5x, cusor_5y);
+      M5.Lcd.print("RELAY_2 STATE IS NOW:");
+      redText();
+      M5.Lcd.print(" OFF");
+      textColortoDefault();
       digitalWrite(RELAY_1, HIGH);
       digitalWrite(RELAY_2, LOW);
-      Serial1.write("<01>"); // send a message back that shows the current state of the relay
-                             // Serial1.flush();
-      Serial.println("RELAY_1 STATE IS NOW: ON");
+      Serial1.write("<01>");
     }
 
     if (!strcmp(receivedChars, "10"))
     { // if <10> is sent: relay 2 is on and relay 1 is off
-      M5.Lcd.setCursor(10, 70);
-      M5.Lcd.print("RELAY_2 STATE IS NOW:  ON");
-      M5.Lcd.setCursor(10, 80);
-      M5.Lcd.print("RELAY_1 STATE IS NOW: OFF");
+      M5.Lcd.setCursor(cusor_5x, cusor_5y);
+      M5.Lcd.print("RELAY_2 STATE IS NOW:");
+      greenText();
+      M5.Lcd.print("  ON");
+      textColortoDefault();
+      M5.Lcd.setCursor(cusor_4x, cusor_4y);
+      M5.Lcd.print("RELAY_1 STATE IS NOW:");
+      redText();
+      M5.Lcd.print(" OFF");
+      textColortoDefault();
       digitalWrite(RELAY_2, HIGH);
       digitalWrite(RELAY_1, LOW);
-      Serial1.write("<10>"); // send a message back that shows the current state of the relay
-                             // Serial1.flush();
+      Serial1.write("<10>");
       Serial.println("RELAY_2 STATE IS NOW: ON");
     }
 
     if (!strcmp(receivedChars, "f"))
-    { // if <f> is sent: flusg Serial1 Write
+    { // if <f> is sent: flush Serial1 Write
       Serial1.flush();
     }
 
